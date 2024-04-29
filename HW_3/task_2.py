dfa66a33576654249c308125e87fc1413d0b3f58
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2024-04-29 09:59:01 +0200

    Update task_2.py

--- HW_3/task_2.py
+++ HW_3/task_2.py
@@ -1,37 +1,42 @@
 import turtle
 
-def koch_snowflake(t, order, size):
-
-    if order == 0:
-        t.forward(size)
+def koch_curve(t, length, depth):
+    if depth == 0:
+        t.forward(length)
     else:
-        for angle in [60, -120, 60, 0]:
-            koch_snowflake(t, order - 1, size / 3)
-            t.left(angle)
+        koch_curve(t, length / 3, depth - 1)
+        t.left(60)
+        koch_curve(t, length / 3, depth - 1)
+        t.right(120)
+        koch_curve(t, length / 3, depth - 1)
+        t.left(60)
+        koch_curve(t, length / 3, depth - 1)
+
+def draw_koch_snowflake(t, length, depth):
+    for _ in range(3):
+        koch_curve(t, length, depth)
+        t.right(120)
 
 def main():
-
-    try:
-        recursion_level = int(input("Введіть рівень рекурсії для сніжинки Коха: "))
-    except ValueError:
-        print("Будь ласка, введіть ціле число.")
-        return
-
-
     screen = turtle.Screen()
-    screen.title("Сніжинка Коха")
-    snowflake_turtle = turtle.Turtle()
-    snowflake_turtle.speed(10)
+    screen.setup(width=800, height=800)
 
-    snowflake_turtle.penup()
-    snowflake_turtle.goto(-150, 90)
-    snowflake_turtle.pendown()
 
-    for _ in range(3):
-        koch_snowflake(snowflake_turtle, recursion_level, 300)
-        snowflake_turtle.right(120)
+    t = turtle.Turtle()
+    t.speed(0) 
 
-    screen.exitonclick()
+ 
+    recursion_depth = int(input("Введіть рівень рекурсії для сніжинки Коха: "))
+
+ 
+    t.penup()
+    t.goto(-150, 90)
+    t.pendown()
+    draw_koch_snowflake(t, 300, recursion_depth)
+
+
+    t.hideturtle()
+    screen.mainloop()
 
 if __name__ == "__main__":
     main()
